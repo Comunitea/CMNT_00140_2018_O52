71e8d1ca682796b7aa91d55d5a3134ba28cfa427
javierjcf <javierjcf@gmail.com>
2018-07-12 17:44:22 +0200

    [ADD]o52_custom: Módulo para personalizaciones o52

---
+++ project-addons/o52_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'o52 Custom',
+    'version': '11.0.1.0.0',
+    'summary': 'Adds customizations.',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+}
